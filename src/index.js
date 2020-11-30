e366ccc924a75ea2a66dbbaf47f395fcde02440a
Ignacio Del Busto <gidelbusto@gmail.com>
2020-11-30 18:12:40 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
   const createTaskForm = document.getElementById("create-task-form")
-  const userInputTaskDescription = document.getElementById("new-task-description")
+  const userInput = document.getElementById("new-task-description")
   const taskList = document.getElementById("tasks")
   
   userInputTaskDescription.addEventListener('submit', 'submitTaks')
   
   Function submitTaks(submit) {
     submit.preventDefault();
-    const newItem = document.createElement("li"); 
+    const newItem = document.createElement("li");
+    newItem.innerText = userInput.value;
+    taskList.appendChild(newItem);
   }
   
   
