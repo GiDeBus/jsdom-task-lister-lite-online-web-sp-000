ffe16b768bfd565a538203bd315fa2e95181ad93
Ignacio Del Busto <gidelbusto@gmail.com>
2020-11-30 18:02:49 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
   const createTaskForm = document.getElementById("create-task-form")
-  const userInputDescription = document.getElementById("new-task-description")
+  const userInputTaskDescription = document.getElementById("new-task-description")
   const taskList = document.getElementById("tasks")
   
   
