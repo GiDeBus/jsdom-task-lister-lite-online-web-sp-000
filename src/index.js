3447ca32c81c836ed08202601814d37a973f349b
Ignacio Del Busto <gidelbusto@gmail.com>
2020-11-30 17:52:32 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const createTaskForm = document.getElementById("create-task-form")
+  const taskDescription = document.getElementById("new-task-description")
+  const taskList = document.getElementById("tasks")
 });
